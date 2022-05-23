32b00057a464e3bd7c1ceb84e37e64ad55b6501d
Shoab <mohammed.shoab@epita.fr>
2022-05-23 10:29:55 +0200

    Added db files and api returns list

---
+++ Api/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+
+engine=create_engine("postgresql://postgres:postgres@localhost/car_preds",
+    echo=True
+)
+
+Base=declarative_base()
+
+SessionLocal=sessionmaker(bind=engine)
--- Api/main.py
+++ Api/main.py
@@ -1,4 +1,5 @@
 import joblib
+import json
 import uvicorn
 import numpy as np
 import pandas as pd
@@ -6,31 +7,68 @@ from pydantic import BaseModel
 from used_cars.inference import make_predictions
 from fastapi import FastAPI, File, UploadFile
 from fastapi import FastAPI
+from Car import Car
 
 app = FastAPI(title='Car Price Prediction', version='1.0',
               description='')
 
+db = SessionLocal()
 
 @app.get('/')
 @app.get('/home')
 def read_home():
     return {'message': 'Thank you for using our car price prediction app'}
 
+# When the feature values are received through a file
+
 @app.post("/predict")
 def predict(csv_file:UploadFile):
     user_data_df = pd.read_csv(csv_file.file,encoding='latin-1')
+    
     predictions = make_predictions(user_data_df)
+    user_data_df = user_data_df.fillna("")
+    f_df = user_data_df.join(pd.DataFrame(predictions,columns=['Price']))
+    
+    return f_df.values.tolist()
 
-    #convertToDF(user_data_df,predictions.tolist())
-    # # #return {'message':predictions.tolist()}
-    return predictions.tolist()
+# When the feature values are received through a request
 
-    #return {'message':predictions[0]}
+@app.post("/predictSingle")
+def predict(req:Car):
+    dateCrawled:req.dateCrawled
+    name:req.name
+    name:req.name
+    offerType:req.offerType
+    abtest:req.abtest
+    vehicleType:req.vehicleType
+    yearOfRegistration:req.yearOfRegistration
+    gearbox:req.gearbox
+    powerPS:req.powerPS
+    model:req.model
+    kilometer:req.kilometer
+    monthOfRegistration:req.monthOfRegistration
+    fuelType:req.fuelType
+    brand:req.brand
+    notRepairedDamage:req.notRepairedDamage
+    dateCreated:req.dateCreated
+    nrOfPictures:req.nrOfPictures
+    postalCode:req.postalCode
+    lastSeen:req.lastSeen
+  
+    predictions = make_predictions(user_data_df)
+    user_data_df = user_data_df.fillna("")
+    f_df = user_data_df.join(pd.DataFrame([predictions[0]],columns=['Price']))
+    
+    return f_df.values.tolist()
 
-def convertToDF(user_data_df,predictions):
-    #df= user_data_df.append(pd.DataFrame(predictions))
-    df = pd.DataFrame(predictions)
-    return df
+
+@app.get('/predHistory')
+def get_all_preds():
+    pass
+
+#@app.post('/predict')
+#def add_pred(userdf,car:Car):
+#    pass
 
     
 if __name__ == '__main__':
